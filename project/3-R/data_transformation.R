6d8978102dfcb7527172994775b46777227c1f5e
oskval <oskaras.valentinavicius@gmail.com>
2022-12-20 01:19:28 +0200

    Data join

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,3 +1,11 @@
+install.packages("tidyverse")
 library(readr)
-read_csv("../1-data/put_your_data_files_here.csv")
-### save combined file into 1-data directory
+library(tidyverse)
+
+data1 <- read_csv("../1-data/1-sample_data.csv")
+data2 <- read_csv("../1-data/2-additional_data.csv")
+additionalFeatures <- read_csv("../1-data/3-additional_features.csv")
+
+joined_data <- inner_join(data, data_additional, by = "id")
+
+write_csv(data_full, "../../../project/1-data/train_data.csv")
