051a7d77f6ce646d6808e9c342df55c9e4d23902
Oskaras Valentinavičius <oskaras.valentinavicius@devbridge.com>
2022-12-26 15:34:58 +0200

    added model code

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -6,6 +6,7 @@ data1 <- read_csv("../1-data/1-sample_data.csv")
 data2 <- read_csv("../1-data/2-additional_data.csv")
 additionalFeatures <- read_csv("../1-data/3-additional_features.csv")
 
-joined_data <- inner_join(data, data_additional, by = "id")
+alldata <- full_join(data1, data2)
+joined_data <- inner_join(alldata, additionalFeatures, by = "id")
 
-write_csv(data_full, "../../../project/1-data/train_data.csv")
+write_csv(joined_data, "../1-data/train_data.csv")
--- project/3-R/modelling.R
+++ project/3-R/modelling.R
@@ -1,2 +1,39 @@
+
 library(h2o)
-h2o.init()
+library(readr)
+h2o.init(max_mem_size = "6g")
+
+df <- h2o.importFile("C:/Users/oskaras.valentinavic/Desktop/SMD S01E01/SMD S01E01/P160M132 Fuck/Project/KTU-DVDA-PROJECT/project/1-data/train_data.csv")
+test_data <- h2o.importFile("C:/Users/oskaras.valentinavic/Desktop/SMD S01E01/SMD S01E01/P160M132 Fuck/Project/KTU-DVDA-PROJECT/project/1-data/test_data.csv")
+df
+
+class(df)
+summary(df)
+
+y <- "y"
+x <- setdiff(names(df), c(y, "id"))
+df$y <- as.factor(df$y)
+summary(df)
+
+splits <- h2o.splitFrame(df, c(0.7, 0.2), seed  = 1234)
+train <- h2o.assign(splits[[1]], "train")
+valid <- h2o.assign(splits[[2]], "valid")
+test <- h2o.assign(splits[[3]], "test")
+
+# automl
+drf_model <- h2o.randomForest(x = x,
+                             y = y,
+                             sample_rate = 0.8,
+                             col_sample_rate_per_tree = 0.8,
+                             ntrees = 70,
+                             max_depth = 50,
+                             min_rows = 20,
+                             calibrate_model = TRUE,
+                             calibration_frame = valid,
+                             binomial_double_trees = TRUE,
+                             training_frame = train,
+                             validation_frame = valid)
+drf_model
+
+h2o.performance(drf_model, valid)
+
